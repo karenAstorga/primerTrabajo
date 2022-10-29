a8f95bec132c8200f02eaf42b9de93b580e5fc63
karenAstorga <114182867+karenAstorga@users.noreply.github.com>
2022-10-29 15:55:25 -0300

    Agregando html de tickets y mejorando el header.

---
+++ javascriptTickets.js
@@ -0,0 +1,59 @@
+
+ let cantidad= document.getElementById("validationCantidad")
+ let categoria= document.getElementById("validationCategoria")
+ let cuentaTotal= document.getElementById("totalPagar")
+ let btnBorrar= document.getElementById("borrar")
+ let btnResumen= document.getElementById("resumen")
+ 
+ var general= 200
+ var estudiante= general*0.2
+ var trainee= general*0.5
+ var junior= general*0.85
+ var total= 0
+ console.log(estudiante)
+
+ function calculaTicket() {
+
+    var ticket= categoria.value;
+    var cuantos= cantidad.value;
+    cuantos= parseInt(cuantos)
+
+    switch (ticket) {
+        case "Estudiante":
+
+            total= cuantos*estudiante
+
+            break;
+        case "Trainee":
+
+            total= cuantos*trainee
+            break;
+        case "Junior":
+
+            total= cuantos*junior
+            break;
+    
+        default:
+
+            total= cuantos*general
+            
+    }
+
+    return ("Total a pagar $"+ total)
+    
+ }
+
+ function insertaTotal() {
+
+    cuentaTotal.value= calculaTicket();
+    
+ }
+
+function borraCampos() {
+
+    location.reload();
+    
+}
+
+ btnResumen.onclick= insertaTotal;
+ btnBorrar.onclick= borraCampos;
